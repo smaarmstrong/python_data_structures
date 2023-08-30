7e0e6a010650f6b06d9c440d1628f254b17730e6
Sean Armstrong <seanmaarmstrong@gmail.com>
2023-08-30 10:22:06 -0700

    merge two singly linked lists

--- singly_linked_list_merge_two.py
+++ singly_linked_list_merge_two.py
@@ -192,22 +192,36 @@ class LinkedList:
             else:
                 s = q
                 q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+        return new_head
 
 llist_1 = LinkedList()
 llist_2 = LinkedList()
 
-llist.append(1)
-llist.append(5)
-llist.append(7)
-llist.append(9)
-llist.append(10)
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
 
-llist.append(2)
-llist.append(3)
-llist.append(4)
-llist.append(6)
-llist.append(8)
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
 
 llist_1.merge_sorted(llist_2)
-
-llist.print_list()
+llist_1.print_list()
