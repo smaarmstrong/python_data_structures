0c80ddf6ac4107e145d99bf8607552a039f3c2d3
smaarmstrong <seanmaarmstrong@gmail.com>
2023-09-14 11:57:19 -0700

    wrap-up of the learning, self-made quiz forthcoming

---
+++ barrett/9-O-Wrap-Up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# suppose n = 100, what do the big 4 O operations equal?
+# O(1) = 1
+# O(log n) ≈ 7
+# O(n) = 100
+# O(n^2) = 1,000,000
+# this is a way of visualizing the efficiency between these O cases
+
+# O(n^2) is a loop within a loop
+# O(n) is proportional
+# O(log n) is divide and conquer
+# O(1) is constant time
+
+# bigocheatsheet.com is a good reference for this stuff
