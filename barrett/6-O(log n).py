6e509a9efd79f406d458fb31226bde3505a3bcf4
smaarmstrong <seanmaarmstrong@gmail.com>
2023-09-14 10:02:29 -0700

    O(log n)

---
+++ barrett/6-O(log n).py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# O(log n) is almost as efficient as O(1)
+
+# O(n log n) is also used sometimes for sorting algorithms,
+# but it is very inefficient, though not as bad as O(n^2)
