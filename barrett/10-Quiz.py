95f08379fa9d24346af37eba2e29a244f5c12a44
smaarmstrong <seanmaarmstrong@gmail.com>
2023-09-14 15:32:03 -0700

    wrote a quiz for big O

---
+++ barrett/10-Quiz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# What is the second worst O case? Write an example of it.
+
+# How can we simplify O(2n)? Write an example of O(2n).
+
+# What is the least efficient O case?
+
+# How can we simplify O(n^2 + n)? Write an example of O(n^2 + n).
+
+# What is the most efficient O case? What is this case also known as? 
+# Write an example of this case.
+
+# What is the second best O case? 
+
+# What is the O case occasionally used for sorting algorithms?
+
+# Write an example of O(n).
+
+# Write an example of O(a + b).
+
+# Write an example of O(a * b).
+
+# List two examples of list operations that are O(1). Why are they O(1)?
+
+# List two examples of list operations that are O(n). Why are they O(n)?
+
+# When you iterate through a list to find an item, what O case is that?
+
+# When you find an item through o_list[n], what O case is that?
+
+# Suppose n = 100, what do the big 4 O cases equal?
+
+# Describe the big 4 O cases with a few words.
+
+# What is a good reference website for O cases?
