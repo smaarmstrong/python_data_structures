9b49b669514e0653d6f80cbda9f9e6f77d3e918c
smaarmstrong <seanmaarmstrong@gmail.com>
2024-01-31 14:45:55 -0800

    Start linked lists in python.

---
+++ barrett/3_Linked_Lists/1-ll-intro.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# There are some key differences between a list in python
+# and a Linked List.
+
+# 1) Linked Lists don't have indices.
+# 2) Linked Lists nodes are not contiguous in memory.
+# 3) Linked List heads are the first node in a linked list.
+# 4) Linked List tails are the last node in a linked list.
+# 5) Heads point to the next item in the list and so on until the
+# penultimate node points to the tail. The tail points to None.
